af28b22c1e8e2f57034d153fdef64c26c35ff6bf
VinniGSBP <gs.vinni@gmail.com>
2024-03-19 19:58:55 -0300

    novo comentario em program

--- Aula42/Program.cs
+++ Aula42/Program.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 
 namespace Course {
     class Program {
+        // versao atualizada do comentario aula git hub
         static void Main(string[] args) {
 
             Produto p = new Produto("TV", 500.00, 10);
