f4dc15d4149c5cf020f2e1023c6bf4d0465ad497
VinniGSBP <gs.vinni@gmail.com>
2024-03-19 19:38:11 -0300

    Aula 42 udemy C#

---
+++ Aula42/Produto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula42 {
+    internal class Produto {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+        
+
+        public Produto() {
+            Quantidade = 10;
+        }
+
+        public Produto(string nome, double preco) : this() {
+            Nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
+            Quantidade = quantidade;
+        }
+
+       public string Nome {
+            get {
+                return _nome; 
+            }
+            set {
+                if (value != null && Nome.Length > 1)
+                    _nome = value;
+            }
+        }
+       
+           
+
+        public double ValorTotalEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarEstoque(int quantidade) {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverEstoque(int quantidade) {
+            Quantidade = Quantidade - quantidade;
+        }
+
+
+
+
+        public override string ToString() {
+            return _nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
